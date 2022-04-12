5529d8d945073ddbc27814c8b7a328807a0f74f8
Randy <rtang@wyze.com>
2022-04-12 10:38:34 -0700

    define library call back

--- src/main/java/com/sweet/plugin/sample/SampleMainActivity.kt
+++ src/main/java/com/sweet/plugin/sample/SampleMainActivity.kt
@@ -1,19 +1,25 @@
 package com.sweet.plugin.sample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sweet.plugin.sample.databinding.ActivitySampleMainBinding
 
 class SampleMainActivity : AppCompatActivity() {
-
     private var activitySampleMainBinding: ActivitySampleMainBinding? = null
     private val binding get() = activitySampleMainBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activitySampleMainBinding = ActivitySampleMainBinding.inflate(layoutInflater)
         setContentView(activitySampleMainBinding?.root)
-        binding.pluginInfoTv.text =
-            "Plugin info: \nname:${BuildConfig.PLUGIN_NAME}, version:${BuildConfig.VERSION_NAME},"
+        "Plugin info: \nname:${BuildConfig.PLUGIN_NAME}, version:${BuildConfig.VERSION_NAME},".also { binding.pluginInfoTv.text = it }
+
+        binding.doneB.setOnClickListener {
+            val doneResult = Intent()
+            doneResult.putExtra("PLUGIN_DONE", SampleMainActivity::class.simpleName)
+            setResult(SampleMainActivity::class.qualifiedName.hashCode(), doneResult)
+            finish()
+        }
     }
 
     override fun onDestroy() {
